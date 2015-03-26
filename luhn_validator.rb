9ce9439563d06ddb014f369b8e928f53db3b60fe
Mark Noble <noble.m.a@gmail.com>
2015-03-26 15:36:59 +0800

    4th Commit, Fix again in luhn_validator.rb, added to the final calculation brackets so it subtracts 10 first then modulos

--- luhn_validator.rb
+++ luhn_validator.rb
@@ -10,7 +10,7 @@ module LuhnValidator
     sum = nums_a.reverse.each_slice(2).map do |x, y|
     	[(x * 2).divmod(10), y || 0]
     	end.flatten.inject(:+)
-    (10 - sum % 10) == check # implicit return
+    ((10 - sum) % 10) == check # implicit return
 
     # TODO: use the integers in nums_a to validate its last check digit
   end
--- test.rb
+++ test.rb
@@ -6,14 +6,21 @@ def credit_card_valid?(account_number)
   #nums_a = stringnum.to_s.chars.map(&:to_i) # SS start code
 
   digits = account_number.to_s.chars.map(&:to_i)
+  puts "Original digits are: ", digits.join
   check = digits.pop
+  puts "check digit is ", check
+  puts "modified digits are: ", digits.join
 
   sum = digits.reverse.each_slice(2).map do |x, y|
     [(x * 2).divmod(10), y || 0]
   end.flatten.inject(:+)
-
-  (10 - sum % 10) == check
+  puts "sum before final clac is: ",sum
+  ans = ((10 - sum) % 10) 
+  puts "afer calc final ans is: ", ans
+  ans == check
 end
 
-puts credit_card_valid?("79927398713")
-puts credit_card_valid?("79927398716")
+#puts credit_card_valid?("79927398713")
+#puts credit_card_valid?("6011732236366813")
+puts credit_card_valid?("6011368353866440"), " expected true"
+#puts credit_card_valid?("346350675600102")
